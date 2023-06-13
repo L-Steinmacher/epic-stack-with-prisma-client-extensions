12bebe6dbe5d37b847bfcfa508d230b03cbcb83b
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-06-13 08:50:32 -0700

    Init

---
+++ app/root.tsx
@@ -0,0 +1,251 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import {
+	json,
+	type DataFunctionArgs,
+	type LinksFunction,
+	type V2_MetaFunction,
+	type HeadersFunction,
+} from '@remix-run/node'
+import {
+	Form,
+	Link,
+	Links,
+	LiveReload,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+	useLoaderData,
+	useSubmit,
+} from '@remix-run/react'
+import { withSentry } from '@sentry/remix'
+import { ThemeSwitch, useTheme } from './routes/resources+/theme.tsx'
+import tailwindStylesheetUrl from './styles/tailwind.css'
+import fontStylestylesheetUrl from './styles/font.css'
+import { authenticator, getUserId } from './utils/auth.server.ts'
+import { ClientHintCheck, getHints } from './utils/client-hints.tsx'
+import { prisma } from './utils/db.server.ts'
+import { getEnv } from './utils/env.server.ts'
+import { ButtonLink } from './utils/forms.tsx'
+import { getDomainUrl } from './utils/misc.server.ts'
+import { getUserImgSrc } from './utils/misc.ts'
+import { useNonce } from './utils/nonce-provider.ts'
+import { getSession, getTheme } from './utils/session.server.ts'
+import { useOptionalUser, useUser } from './utils/user.ts'
+import { makeTimings, time } from './utils/timing.server.ts'
+
+export const links: LinksFunction = () => {
+	return [
+		// Preload CSS as a resource to avoid render blocking
+		{ rel: 'preload', href: fontStylestylesheetUrl, as: 'style' },
+		{ rel: 'preload', href: tailwindStylesheetUrl, as: 'style' },
+		cssBundleHref ? { rel: 'preload', href: cssBundleHref, as: 'style' } : null,
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			href: '/favicons/apple-touch-icon.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '32x32',
+			href: '/favicons/favicon-32x32.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/png',
+			sizes: '16x16',
+			href: '/favicons/favicon-16x16.png',
+		},
+		{ rel: 'manifest', href: '/site.webmanifest' },
+		{ rel: 'icon', href: '/favicon.ico' },
+		{ rel: 'stylesheet', href: fontStylestylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
+	].filter(Boolean)
+}
+
+export const meta: V2_MetaFunction = () => {
+	return [
+		{ title: 'Epic Notes' },
+		{ name: 'description', content: 'Find yourself in outer space' },
+	]
+}
+
+export async function loader({ request }: DataFunctionArgs) {
+	const cookieSession = await getSession(request.headers.get('Cookie'))
+	const timings = makeTimings('root loader')
+	const userId = await time(() => getUserId(request), {
+		timings,
+		type: 'getUserId',
+		desc: 'getUserId in root',
+	})
+
+	const user = userId
+		? await time(
+				() =>
+					prisma.user.findUnique({
+						where: { id: userId },
+						select: { id: true, name: true, username: true, imageId: true },
+					}),
+				{ timings, type: 'find user', desc: 'find user in root' },
+		  )
+		: null
+	if (userId && !user) {
+		console.info('something weird happened')
+		// something weird happened... The user is authenticated but we can't find
+		// them in the database. Maybe they were deleted? Let's log them out.
+		await authenticator.logout(request, { redirectTo: '/' })
+	}
+
+	return json(
+		{
+			user,
+			requestInfo: {
+				hints: getHints(request),
+				origin: getDomainUrl(request),
+				path: new URL(request.url).pathname,
+				session: {
+					theme: getTheme(cookieSession),
+				},
+			},
+			ENV: getEnv(),
+		},
+		{
+			headers: {
+				'Server-Timing': timings.toString(),
+			},
+		},
+	)
+}
+
+export const headers: HeadersFunction = ({ loaderHeaders }) => {
+	const headers = {
+		'Server-Timing': loaderHeaders.get('Server-Timing') ?? '',
+	}
+	return headers
+}
+
+function App() {
+	const data = useLoaderData<typeof loader>()
+	const nonce = useNonce()
+	const user = useOptionalUser()
+	const theme = useTheme()
+
+	return (
+		<html lang="en" className={`${theme} h-full`}>
+			<head>
+				<ClientHintCheck nonce={nonce} />
+				<Meta />
+				<meta charSet="utf-8" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<Links />
+			</head>
+			<body className="flex h-full flex-col justify-between bg-day-300 text-white dark:bg-night-700 ">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<Link to="/">
+							<div className="font-light text-black dark:text-white">epic</div>
+							<div className="font-bold text-black dark:text-white">notes</div>
+						</Link>
+						<div className="flex items-center gap-10">
+							{user ? (
+								<UserDropdown />
+							) : (
+								<ButtonLink to="/login" size="sm" variant="primary">
+									Log In
+								</ButtonLink>
+							)}
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<Link to="/">
+						<div className="font-light text-black dark:text-white">epic</div>
+						<div className="font-bold text-black dark:text-white">notes</div>
+					</Link>
+					<ThemeSwitch userPreference={data.requestInfo.session.theme} />
+				</div>
+				<div className="h-5" />
+				<ScrollRestoration nonce={nonce} />
+				<Scripts nonce={nonce} />
+				<script
+					nonce={nonce}
+					dangerouslySetInnerHTML={{
+						__html: `window.ENV = ${JSON.stringify(data.ENV)}`,
+					}}
+				/>
+				<LiveReload nonce={nonce} />
+			</body>
+		</html>
+	)
+}
+export default withSentry(App)
+
+function UserDropdown() {
+	const user = useUser()
+	const submit = useSubmit()
+	return (
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild>
+				<Link
+					to={`/users/${user.username}`}
+					// this is for progressive enhancement
+					onClick={e => e.preventDefault()}
+					className="flex items-center gap-2 rounded-full bg-night-500 py-2 pl-2 pr-4 outline-none hover:bg-night-400 focus:bg-night-400 radix-state-open:bg-night-400"
+				>
+					<img
+						className="h-8 w-8 rounded-full object-cover"
+						alt={user.name ?? user.username}
+						src={getUserImgSrc(user.imageId)}
+					/>
+					<span className="text-body-sm font-bold text-white">
+						{user.name ?? user.username}
+					</span>
+				</Link>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Portal>
+				<DropdownMenu.Content
+					sideOffset={8}
+					align="start"
+					className="flex flex-col rounded-3xl bg-[#323232]"
+				>
+					<DropdownMenu.Item asChild>
+						<Link
+							prefetch="intent"
+							to={`/users/${user.username}`}
+							className="rounded-t-3xl px-7 py-5 outline-none hover:bg-night-500 radix-highlighted:bg-night-500"
+						>
+							Profile
+						</Link>
+					</DropdownMenu.Item>
+					<DropdownMenu.Item asChild>
+						<Link
+							prefetch="intent"
+							to={`/users/${user.username}/notes`}
+							className="px-7 py-5 outline-none hover:bg-night-500 radix-highlighted:bg-night-500"
+						>
+							Notes
+						</Link>
+					</DropdownMenu.Item>
+					<DropdownMenu.Item asChild>
+						<Form
+							action="/logout"
+							method="POST"
+							className="rounded-b-3xl px-7 py-5 outline-none radix-highlighted:bg-night-500"
+							onClick={e => submit(e.currentTarget)}
+						>
+							<button type="submit">Logout</button>
+						</Form>
+					</DropdownMenu.Item>
+				</DropdownMenu.Content>
+			</DropdownMenu.Portal>
+		</DropdownMenu.Root>
+	)
+}
