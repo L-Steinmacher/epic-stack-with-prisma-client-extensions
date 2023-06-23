50e21506e41b19e1110b510fcee6314a772b2497
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2023-06-22 19:53:51 -0700

    bug: fixed import

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import { createPassword, createUser } from 'tests/db-utils.ts'
 import { prisma } from '~/utils/db.server.ts'
 import { deleteAllData } from 'tests/setup/utils.ts'
 import { getPasswordHash } from '~/utils/auth.server.ts'
-import { PriorityEnum, Note } from '~/models/note.ts'
+import { PriorityEnum } from 'types/priority.ts'
 
 async function seed() {
 	console.log('🌱 Seeding...')
